0e1f13f511406372a0a709ff76393bc6d0b4a118
ValerianoBenitesJuan <jvalerianob1@upao.edu.pe>
2024-09-18 22:50:45 -0500

    Implementacion de busqueda de tutores por materia

---
+++ aprendeya-api/src/main/java/com/aprendeya/aprendeyaapi/repository/SesionRepository.java
@@ -0,0 +1,16 @@
+package com.aprendeya.aprendeyaapi.repository;
+
+import com.aprendeya.aprendeyaapi.model.entity.Curso;
+import com.aprendeya.aprendeyaapi.model.entity.Sesion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface SesionRepository extends JpaRepository<Sesion, Integer>  {
+
+    @Query("SELECT s FROM Sesion s  WHERE s.curso=:curso")
+    List<Sesion> buscarSesionesPorCurso(@Param("curso") Curso curso);
+
+}
--- aprendeya-api/src/main/java/com/aprendeya/aprendeyaapi/service/impl/TutorService.java
+++ aprendeya-api/src/main/java/com/aprendeya/aprendeyaapi/service/impl/TutorService.java
@@ -4,12 +4,19 @@ import com.aprendeya.aprendeyaapi.dto.TutorPerfilRequestDTO;
 import com.aprendeya.aprendeyaapi.dto.TutorResponseDTO;
 import com.aprendeya.aprendeyaapi.exception.ResourceNotFoundException;
 import com.aprendeya.aprendeyaapi.mapper.TutorMapper;
+import com.aprendeya.aprendeyaapi.model.entity.Curso;
+import com.aprendeya.aprendeyaapi.model.entity.Sesion;
 import com.aprendeya.aprendeyaapi.model.entity.Tutor;
+import com.aprendeya.aprendeyaapi.repository.CursoRepository;
+import com.aprendeya.aprendeyaapi.repository.SesionRepository;
 import com.aprendeya.aprendeyaapi.repository.TutorRepository;
 import jakarta.persistence.EntityManager;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
@@ -17,6 +24,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class TutorService {
 
     private final TutorRepository tutorRepository;
+    private final CursoRepository cursoRepository;
+    private final SesionRepository sesionRepository;
     private final TutorMapper tutorMapper;
     private final EntityManager entityManager;
 
@@ -33,5 +42,30 @@ public class TutorService {
         Tutor tutorActualizado = tutorRepository.buscarTutorPorID(idTutor);
 
         return tutorMapper.convertToDTO(tutorActualizado);
+    }
+
+    @Transactional
+    public List<TutorResponseDTO> buscarTutoresPorMateria(String nombreCurso){
+        Curso curso = cursoRepository.buscarCursoPorNombre(nombreCurso);
+        List<Sesion> sesiones;
+        List<Tutor> tutores = new ArrayList<>();
+
+        if(curso == null){
+            throw new ResourceNotFoundException("No existe el curso");
+        }
+        else{
+            sesiones = sesionRepository.buscarSesionesPorCurso(curso);
+
+            if(sesiones.isEmpty()){
+                throw new ResourceNotFoundException("No hay tutores para este curso");
+            }
+            else{
+                for(Sesion sesion : sesiones){
+                    Tutor tutorNuevo = tutorRepository.buscarTutorPorID(sesion.getTutor().getIdTutor());
+                    tutores.add(tutorNuevo);
+                }
+            }
+        }
+        return tutorMapper.convertToListDTO(tutores);
     }
 }
